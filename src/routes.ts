b58502650e23b64e86938213c03bd1d9a29394d5
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-11-11 23:33:03 -0300

    fix: remove unused files

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,6 @@ import uploadConfig from './config/multer';
 
 import ProductsController from './controllers/ProductsController';
 import OrderController from './controllers/OrderController';
-import ImagesController from './controllers/ImagesController';
 
 const routes = Router();
 const upload = multer(uploadConfig);
@@ -15,10 +14,6 @@ routes.get('/products/:id', ProductsController.show );                          
 routes.post('/products', upload.array('images'), ProductsController.create);    //adicionar novo produto ao estoque
 routes.put('/products', ProductsController.update);                             //atualiza estoque apos uma compra
 routes.delete('/products/:id', ProductsController.delete);                      //retira do estoque um produto
-
-routes.post('/images', upload.array('image'), ImagesController.create);          //adicionar uma nova foto
-routes.get('/images', ImagesController.index);                                   //mostrar info das fotos
-routes.delete('/images/:id', ImagesController.delete);                           //apagar uma foto
 
 routes.post('/checkout', OrderController.create );                              //finalizar a compra
 
