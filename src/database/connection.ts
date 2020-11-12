3defd43e8777387a898eba61d49f67de84c4dc25
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-11-11 23:41:32 -0300

    fix files to deploy

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,44 +1,15 @@
-import knex from 'knex';
-import path from 'path';
+import knex from 'knex'
 import 'dotenv/config'
 
-let db: knex<any, unknown[]>
-
-switch (process.env.NODE_ENV) {
-  case 'development':
-    db = knex({
-      client: 'sqlite3',
-      connection: {
-        filename: path.resolve(__dirname, 'database.sqlite')
-      },
-      useNullAsDefault: true,
-    });
-    break;
-
- case 'test':
-    db = knex({
-      client: 'sqlite3',
-      connection: {
-        filename: path.resolve(__dirname, 'teste.sqlite')
-      },
-      useNullAsDefault: true,
-    });
-    break;
-
-  case 'production':
-  default:
-    db = knex({
-      client: 'pg',
-      connection: {
-        host : process.env.PG_HOST,
-        user : process.env.PG_USER,
-        password : process.env.PG_PASSWORD,
-        database : process.env.PG_DATABASE,
-      },
-      useNullAsDefault: true
-    })
-    break;
-
-}
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : process.env.PG_HOST,
+    user : process.env.PG_USER,
+    password : process.env.PG_PASSWORD,
+    database : process.env.PG_DATABASE,
+  },
+  useNullAsDefault: true
+})
 
 export default db;
