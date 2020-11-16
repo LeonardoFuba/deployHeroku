867bd5e571fbf82e1e32677c69bc7785f2b674d9
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-11-16 01:31:03 -0300

    chore: update mvc files and migrations scripts

--- src/routes.ts
+++ src/routes.ts
@@ -9,12 +9,13 @@ import OrderController from './controllers/OrderController';
 const routes = Router();
 const upload = multer(uploadConfig);
 
-routes.get('/products', ProductsController.index );                             //mostrar todos os produtos
-routes.get('/products/:id', ProductsController.show );                          //mostrar um unico produtos detalhado
+routes.get('/products', ProductsController.index);                             //mostrar todos os produtos
+routes.get('/products/:id', ProductsController.show);                          //mostrar um unico produtos detalhado
 routes.post('/products', upload.array('images'), ProductsController.create);    //adicionar novo produto ao estoque
 routes.put('/products', ProductsController.update);                             //atualiza estoque apos uma compra
 routes.delete('/products/:id', ProductsController.delete);                      //retira do estoque um produto
 
-routes.post('/checkout', OrderController.create );                              //finalizar a compra
+routes.post('/orders', OrderController.index);                                //listar pedidos
+routes.post('/checkout', OrderController.create);                              //finalizar a compra
 
 export default routes;
