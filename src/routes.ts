e8c74a31f4e6c02cb134d492be1332d4eab3a4a1
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-12-04 17:42:16 -0300

    fix: fix coreios route to post

--- src/routes.ts
+++ src/routes.ts
@@ -11,17 +11,17 @@ import ShippingController from './controllers/ShippingController';
 const routes = Router();
 const upload = multer(uploadConfig);
 
-routes.get('/products', ProductsController.index);                             //mostrar todos os produtos
-routes.get('/products/:id', ProductsController.show);                          //mostrar um unico produtos detalhado
+routes.get('/products', ProductsController.index);                              //mostrar todos os produtos
+routes.get('/products/:id', ProductsController.show);                           //mostrar um unico produtos detalhado
 routes.post('/products', upload.array('images'), ProductsController.create);    //adicionar novo produto ao estoque
 routes.put('/products', ProductsController.update);                             //atualiza estoque apos uma compra
 routes.delete('/products/:id', ProductsController.delete);                      //retira do estoque um produto
 
-routes.get('/orders', OrderController.index);                                  //listar pedidos
-routes.get('/orders/:id', OrderController.show);                                  //listar pedidos
+routes.get('/orders', OrderController.index);                                   //listar pedidos
+routes.get('/orders/:id', OrderController.show);                                //listar pedidos
 routes.post('/checkout', OrderController.create);                               //finalizar a compra
 
-routes.get('/cep/:cep', ZipCodeController.show);
-routes.get('/preco', ShippingController.index);
+routes.get('/cep/:cep', ZipCodeController.show);																//consulta informacoes sobre o cep
+routes.post('/preco', ShippingController.index);																//calcula frete e prazo dos correios
 
 export default routes;
