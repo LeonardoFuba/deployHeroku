f82f08f3bc53b5e8f389ff9ae32e12a945ac1e8f
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-12-24 00:14:14 -0300

    test(server): cors with multiple origins

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,46 @@
-import 'dotenv/config';
-
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
-import routes from './routes';
 import morgan from 'morgan';
 
+import routes from './routes';
+import errorHandler from './errors/handler';
 
 import 'express-async-errors'
-import errorHandler from './errors/handler';
+import 'dotenv/config';
 
 const app = express();
 
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://alira.vercel.app',
+  'https://df2a9c27470d.ngrok.io'
+];
+
+app.use(cors({
+  origin: (origin='http://alira:3000', callback) => {
+
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+
+    return callback(null, true);
+  }
+}));
+
+// [
+//   'http://localhost:3000',
+//   'http://alira.vercel.app',
+//   'http://aliranotes.com.br',
+//   'http://743d62eba705.ngrok.io'
+// ]
+
+// app.use(cors({ origin: 'http://localhost:3000'          }));
+// app.use(cors({ origin: 'http://743d62eba705.ngrok.io'   }));
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
